937a6d161db58d3815ac253365247986e3f9310e
glatteis <justwitti@gmail.com>
2018-02-27 13:20:35 +0100

    First chat design

--- chat.js
+++ chat.js
@@ -36,26 +36,97 @@ setTimeout(function () {
     webSocket.onmessage = function (ev) {
         var data = JSON.parse(ev.data);
         console.log(data);
+        for (var p in data) {
+            if (data.hasOwnProperty(p) && (typeof data[p]) === "string") {
+                data[p] = escapeHtml(data[p]);
+            }
+        }
         switch (data.type) {
             case "error":
                 $("body").append("<p>This Room does not exist :(</p>");
                 break;
             case "message":
-                addChatMessage(data.username + ": " + data.message);
+                // TODO better check for "self"
+                addChatMessage(data.username, data.message, data.username === username, msToTime(data.time));
                 break;
             case "info-login":
-                addChatMessage(data.username + " logged in!");
+                addChatAlert(data.username + " logged in!");
                 break;
             case "info-logout":
-                addChatMessage(data.username + " logged out!");
+                addChatAlert(data.username + " logged out!");
+                break;
         }
     }
 }, 100);
 
-function addChatMessage(message) {
-    const chatText = $("#chat-window")
-    chatText.val(chatText.val() + "\n" + message);
+
+function msToTime(ms) {
+    return new Date(ms).toISOString().slice(11, 16);
 }
+
+function escapeHtml(text) {
+    console.log(text);
+    var map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
+    };
+
+    return text.replace(/[&<>"']/g, function (m) {
+        return map[m];
+    });
+}
+
+function addChatAlert(message) {
+    const chatDiv = $("#chat-window");
+    const alert = $(
+        "<div class=\"col-sm\">" +
+        "    <span><b>" + message + "</b></span>" +
+        "</div>"
+    );
+    chatDiv.append(alert);
+
+    scrollDown();
+}
+
+function isScrolledDown() {
+    const obj = $("#chat-window")[0];
+    return (obj.scrollTop === Math.max((obj.scrollHeight - obj.offsetHeight) - 100, obj.scrollTop));
+}
+
+function scrollDown() {
+    $(function () {
+        $("#chat-window").scrollTop(1E10);
+    });
+}
+
+
+function addChatMessage(username, message, self, time) {
+    const chatDiv = $("#chat-window");
+
+    const c = self ? "msg-y" : "msg-o";
+
+    const chatMessage = $(
+        "<div class=\"" + c + " col-sm\">\n" +
+        "    <span></span>\n" +
+        "    <span class=\"name\"><b>" + username + "</b></span>\n" +
+        "    <br>\n" +
+        "    <span class=\"message\">" + message + "</span>\n" +
+        "    <span class=\"time\">" + time + "</span>\n" +
+        "</div>\n" +
+        "<div class=\"col-md-6 form-group\"></div>"
+    );
+
+    var scrolledDown = isScrolledDown();
+    chatDiv.append(chatMessage);
+    if (scrolledDown) {
+        scrollDown();
+    }
+}
+
+
 
 
 // Enter press in text input
